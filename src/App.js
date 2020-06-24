f84458af6589b984f0c6e4b2d5ad09df58ce354d
Lilly <lek7hj@virginia.edu>
2020-06-24 10:53:29 -0400

    end of storybook tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './lib/redux';
 
+import InboxScreen from './components/InboxScreen';
+
+import './index.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
   );
 }
-
 export default App;
+
+/*
+POTENTIAL PROBLEM
+rendering containers?
+could
+- never render container components anywhere in your app except at the highest level
+instead pass all data-requirements down the component hierarchy
+OR
+- supply a Redux store to the InboxScreen in a story
+use a mocked version of the Redux store provided in a decorator
+
+*/
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Task from './Task';
+import { connect } from 'react-redux';
+import { archiveTask, pinTask } from '../lib/redux';
 
-function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     const events = {
         onPinTask,
         onArchiveTask,
@@ -55,11 +57,14 @@ function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     );
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
     loading: PropTypes.bool,
     tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
     onPinTask: PropTypes.func.isRequired,
     onArchiveTask: PropTypes.func.isRequired,
+};
+PureTaskList.defaultProps = {
+    loading: false,
 };
 
 export default connect(
@@ -72,4 +77,3 @@ export default connect(
     })
 )(PureTaskList);
 
-export default TaskList;
